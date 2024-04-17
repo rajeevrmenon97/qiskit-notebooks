03526fd39d61a74221940b847b7d93fbb390d7ed
Rajeev Menon <rajeevrmenon97@gmail.com>
2024-04-16 20:22:45 -0600

    Format python file

--- save-credentials.py
+++ save-credentials.py
@@ -1,11 +1,9 @@
 import argparse
 from qiskit_ibm_runtime import QiskitRuntimeService
 
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", help=f"API Token for Qiskit", required=True)
     args = parser.parse_args()
 
-    QiskitRuntimeService.save_account(channel="ibm_quantum", token=args.token, overwrite= True)
+    QiskitRuntimeService.save_account(channel="ibm_quantum", token=args.token, overwrite=True)
